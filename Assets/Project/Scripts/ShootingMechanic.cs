607e588866a5dd3c50e3238fb18a5f270e0b9817
Luan Zaniqueli <luanzaniqueli.carlos@yahoo.com.br>
2022-09-23 23:08:10 -0300

    ft: Adição do player, inimigo e mecânicas básicas.
    Adição do gameobject do player, do inimigo, ajuste das configurações da câmera principal, criação da estrutura de pastas e adição de scripts de mecânicas básicas para o player e o inimigo.

---
+++ Assets/Project/Scripts/ShootingMechanic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ShootingMechanic : MonoBehaviour
+{
+    public GameObject gunObject;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullet.transform.position = gunObject.transform.position + gunObject.transform.forward;
+            bullet.transform.forward = gunObject.transform.forward;
+        }
+    }
+}
